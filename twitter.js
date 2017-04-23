c92d877b5f3101eff46291bced5eae9c7a18dc5c
p.harikesh <p.harikesh409@gmail.com>
2017-04-23 15:01:21 +0530

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -108,5 +108,9 @@ geekonikpanelsetup.value = "";
 
 
 		}
-
+document.write("Copyright &copy 2013 - ");
+var today = new Date();
+var year = today.getFullYear();
+document.write(year);
+document.write(" All Rights Reserved by P.Harikesh @<a href ='http://www.blogg-tricks.blogspot.com/' target='blank'>Blogg-Tricks</a>");
 		//-->
