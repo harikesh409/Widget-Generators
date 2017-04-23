557248a054e259590f6887b3e63412d25f282c97
p.harikesh <p.harikesh409@gmail.com>
2017-04-23 14:42:18 +0530

    twitter

---
+++ copyright.js
@@ -0,0 +1,5 @@
+document.write("Copyright &copy 2013 - ");
+var today = new Date();
+var year = today.getFullYear();
+document.write(year);
+document.write(" All Rights Reserved by P.Harikesh @<a href ='http://www.blogg-tricks.blogspot.com/' target='blank'>Blogg-Tricks</a>");
